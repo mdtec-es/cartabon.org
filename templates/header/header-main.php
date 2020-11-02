f92a5c1b952027dea66566eb1a3fc4aed4f11d9d
donalcazar <alcazarmartinez.a@gmail.com>
2020-11-02 12:15:16 +0100

    Upload Files

---
+++ templates/header/header-main.php
@@ -0,0 +1,25 @@
+<header>
+    <nav class="navbar navbar-expand-sm navbar-toggleable-sm navbar-light bg-white border-bottom box-shadow mb-3">
+        <div class="container">
+            <a class="navbar-brand" href="./"><img class="iconMainTab" src="src/images/cartabon3d/cartabonLogo360240.png" alt="Cartabón - 3D freelancer"></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target=".navbar-collapse" aria-controls="navbarSupportedContent"
+            aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="navbar-collapse collapse d-sm-inline-flex flex-sm-row-reverse">
+            <partial name="_LoginPartial" />
+            <ul class="navbar-nav flex-grow-1">
+                <li class="nav-item">
+                    <a class="nav-link text-dark" href="Login.php">Login</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-dark" href="3D.php">3D Development</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-dark" href="WebDesign.php">Web Design</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+</header>
